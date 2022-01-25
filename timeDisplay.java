b99882d427958224809bb061f293539fa76187a6
Ryan Sim <59084058+RyanSimYH@users.noreply.github.com>
2022-01-25 11:40:22 +0800

    Update timeDisplay.java

--- timeDisplay.java
+++ timeDisplay.java
@@ -7,7 +7,7 @@ public class timeDisplay {
         long totalSeconds = totalMilliseconds/1000;
         long totalMinutes = totalSeconds/60;
         long totalHours = totalMinutes/60;
-        String currentTime = String.format("Current time is: %d:%d:%d GMT",(totalHours%60),(totalMinutes%60),(totalSeconds%60));
+        String currentTime = String.format("Current time is: %d:%d:%d GMT",(totalHours%24),(totalMinutes%60),(totalSeconds%60));
         System.out.println(currentTime);
 
     }
